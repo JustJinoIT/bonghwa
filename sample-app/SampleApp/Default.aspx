96689d1b29c6cfb23d733b08155617015195f852
justjiino <dogfootbro@gmail.com>
2026-07-09 02:05:34 +0900

    test: sample-app — 최소 .NET Framework 4.8 웹앱 (v1)

---
+++ sample-app/SampleApp/Default.aspx
@@ -0,0 +1,7 @@
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SampleApp.Default" %>
+<!DOCTYPE html>
+<html>
+<head><title>Bonghwa Sample App</title></head>
+<body>
+</body>
+</html>
---
+++ sample-app/SampleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SampleApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SampleApp")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
